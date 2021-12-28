48fa0a31017a879999e29a321245b3edeb43528f
djdoc962 <djdoc962@hotmail.com>
2021-12-28 14:27:59 +0800

    minor revision

--- vision/SIFT.py
+++ vision/SIFT.py
@@ -1,8 +1,8 @@
 import numpy as np
-import cv2 as cv
-img = cv.imread('./image/template.jpg')
-gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-sift = cv.SIFT_create()
+import cv2
+img = cv2.imread('./image/template.jpg')
+gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+sift = cv2.SIFT_create() 
 kp = sift.detect(gray,None)
-img=cv.drawKeypoints(gray,kp,img)
-cv.imwrite('sift_keypoints.jpg',img)
+img=cv2.drawKeypoints(gray,kp,img)
+cv2.imwrite('sift_keypoints.jpg',img)
